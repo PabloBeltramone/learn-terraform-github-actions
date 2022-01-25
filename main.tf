9c55a008d903feabd44f09a2e165a7826a1e8134
Pablo Beltramone <pablobeltramone@WKSAR148L.fibertel.com.ar>
2022-01-25 09:54:49 -0300

    point backend to organization

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "acg-tf-demo"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "acg-demo-github-actions"
     }
   }
 }
